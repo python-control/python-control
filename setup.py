fcbe78dc23542147c81f879fe5590c8ab0cf0f8a
Richard Murray <murray@cds.caltech.edu>
2011-04-02 19:32:33 +0000

    added examples/ and tests/ to source distribution; updated version number to 0.4c

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@
 
 from distutils.core import setup
 
-setup(name='control',
-      version='0.4c',
-      description='Python Control Systems Library',
-      author='Richard Murray',
-      author_email='murray@cds.caltech.edu',
-      url='http://python-control.sourceforge.net',
-      requires='scipy',
-      packages=['control'],
-      package_dir = {'control':'src'},
+setup(name = 'control',
+      version = '0.4c',
+      description = 'Python Control Systems Library',
+      author = 'Richard Murray',
+      author_email = 'murray@cds.caltech.edu',
+      url = 'http://python-control.sourceforge.net',
+      requires = ['scipy', 'matplotlib'],
+      package_dir = {'control' : 'src'},
+      packages = ['control'],
      )
