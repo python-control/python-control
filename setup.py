edfd8cec2b7fe849753cf2ae0804f5aabebc8463
Richard Murray <murray@cds.caltech.edu>
2011-06-18 00:20:49 +0000

    updated working version number to 0.4d

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 from setuptools import setup
 
 setup(name = 'control',
-      version = '0.4c',
+      version = '0.4d',
       description = 'Python Control Systems Library',
       author = 'Richard Murray',
       author_email = 'murray@cds.caltech.edu',
