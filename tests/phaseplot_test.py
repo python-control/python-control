a7aed0ae501ff081b710f07da14e864f2ada3cde
Richard Murray <murray@cds.caltech.edu>
2011-07-26 05:44:01 +0000

    updated test suite for phaseplot

--- tests/phaseplot_test.py
+++ tests/phaseplot_test.py
@@ -25,32 +25,33 @@ class TestPhasePlot(unittest.TestCase):
 
     def testInvPendSims(self):
         PhasePlot(self.invpend_ode, (-6,6,10), (-6,6,10), 
-                  xinit = ([1,1], [-1,1]));
+                  X0 = ([1,1], [-1,1]));
 
     def testInvPendTimePoints(self):
         PhasePlot(self.invpend_ode, (-6,6,10), (-6,6,10), 
-                  xinit = ([1,1], [-1,1]), T=np.linspace(0,5,100));
+                  X0 = ([1,1], [-1,1]), T=np.linspace(0,5,100));
 
     def testInvPendLogtime(self):
-        PhasePlot(self.invpend_ode,
-                  'logtime', (3, 0.7), None, 
+        PhasePlot(self.invpend_ode, X0 = 
                   [ [-2*pi, 1.6], [-2*pi, 0.5], [-1.8, 2.1],
                     [-1, 2.1], [4.2, 2.1], [5, 2.1],
                     [2*pi, -1.6], [2*pi, -0.5], [1.8, -2.1],
                     [1, -2.1], [-4.2, -2.1], [-5, -2.1] ], 
-                  np.linspace(0, 40, 200), verbose=False)
+                  T = np.linspace(0, 40, 200),
+                  logtime=(3, 0.7),
+                  verbose=False)
 
     def testInvPendAuto(self):
-        PhasePlot(self.invpend_ode, 'auto', 0, None, 
-                  [[-2.3056, 2.1], [2.3056, -2.1]], 6, verbose=False)
+        PhasePlot(self.invpend_ode, lingrid = 0, X0=
+                  [[-2.3056, 2.1], [2.3056, -2.1]], T=6, verbose=False)
 
     def testOscillatorParams(self):
         m = 1; b = 1; k = 1;			# default values
-        PhasePlot(self.oscillator_ode, 'timepts', [0.3, 1, 2, 3], None,
+        PhasePlot(self.oscillator_ode, timepts = [0.3, 1, 2, 3], X0 = 
                   [[-1,1], [-0.3,1], [0,1], [0.25,1], [0.5,1], [0.7,1],
                    [1,1], [1.3,1], [1,-1], [0.3,-1], [0,-1], [-0.25,-1],
                    [-0.5,-1], [-0.7,-1], [-1,-1], [-1.3,-1]],
-                  np.linspace(0, 10, 100), parms = (m, b, k));
+                  T = np.linspace(0, 10, 100), parms = (m, b, k));
 
     # Sample dynamical systems - inverted pendulum
     def invpend_ode(self, x, t, m=1., l=1., b=0, g=9.8):
