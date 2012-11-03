1599d166aaf803fceaf54ce77147e7d26b2172d2
Richard Murray <murray@cds.caltech.edu>
2012-11-03 04:45:31 +0000

    version 0.6b (pre FRD)

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 from setuptools import setup
 
 setup(name = 'control',
-      version = '0.6a',
+      version = '0.6b',
       description = 'Python Control Systems Library',
       author = 'Richard Murray',
       author_email = 'murray@cds.caltech.edu',
