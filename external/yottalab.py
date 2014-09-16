2dcf1a913124ae7251ec2c97cbaab3a300e5516c
robertobucher <robertobucher@users.noreply.github.com>
2014-09-16 15:39:25 +0200

    Update yottalab.py

--- external/yottalab.py
+++ external/yottalab.py
@@ -6,111 +6,29 @@ roberto.bucher@supsi.ch
 The following commands are provided:
 
 Design and plot commands
-  bb_c2d       - contimous to discrete time conversion
+  dlqr     - Discrete linear quadratic regulator
   d2c         - discrete to continous time conversion
-  bb_dare        - Solve Riccati equation for discrete time systems
-  bb_dlqr        - discrete linear quadratic regulator
-  dsimul      - simulate discrete time systems
-  dstep       - step response (plot) of discrete time systems
-  dimpulse    - imoulse response (plot) of discrete time systems
-  bb_step     - step response (plot) of continous time systems
-  
   full_obs    - full order observer
   red_obs     - reduced order observer
   comp_form   - state feedback controller+observer in compact form
   comp_form_i - state feedback controller+observer+integ in compact form
-  sysctr      - system+controller+observer+feedback
   set_aw      - introduce anti-windup into controller
-  bb_dcgain      - return the steady state value of the step response
+  bb_dcgain   - return the steady state value of the step response
+  placep      - Pole placement (replacement for place)
+
+  Old functions now corrected in python control
+  bb_dare     - Solve Riccati equation for discrete time systems
   
 """
-from matplotlib.pylab import *
-from control import *
-from numpy import hstack,vstack,pi
-from scipy import zeros,ones,eye,mat,shape,size,size, \
-    arange,real,poly,array,diag
-from scipy.linalg import det,inv,expm,eig,eigvals,logm
-import numpy as np
+from numpy import hstack, vstack, rank, imag, zeros, eye, mat, \
+    array, shape, real, sort
+from scipy import poly 
+from scipy.linalg import inv, expm, eig, eigvals, logm
 import scipy as sp
 from slycot import sb02od
-# from scipy.signal import BadCoefficients
-# import warnings
-# warnings.filterwarnings('ignore',category=BadCoefficients)
-
-def bb_c2d(sys,Ts,method='zoh'):
-    """Continous to discrete conversion with ZOH method
-
-    Call:
-    sysd=c2d(sys,Ts,method='zoh')
-
-    Parameters
-    ----------
-    sys :   System in statespace or Tf form 
-    Ts:     Sampling Time
-    method: 'zoh', 'bi' or 'matched'
-
-    Returns
-    -------
-    sysd: ss or Tf system
-    Discrete system
-
-    """
-    flag = 0
-    if isinstance(sys, TransferFunction):
-        sys=tf2ss(sys)
-        flag=1
-
-    a=sys.A
-    b=sys.B
-    c=sys.C
-    d=sys.D
-    n=shape(a)[0]
-    nb=shape(b)[1]
-    nc=shape(c)[0]
-
-    if method=='zoh':
-        ztmp=zeros((nb,n+nb))
-        tmp=hstack((a,b))
-        tmp=vstack((tmp,ztmp))
-        tmp=expm(tmp*Ts)
-        A=tmp[0:n,0:n]
-        B=tmp[0:n,n:n+nb]
-        C=c
-        D=d
-    elif method=='bi':
-        a=mat(a)
-        b=mat(b)
-        c=mat(c)
-        d=mat(d)
-        IT=mat(2/Ts*eye(n,n))
-        A=(IT+a)*inv(IT-a)
-        iab=inv(IT-a)*b
-        tk=2/sqrt(Ts)
-        B=tk*iab
-        C=tk*(c*inv(IT-a))
-        D=d+c*iab
-    elif method=='matched':
-        if nb!=1 and nc!=1:
-            print "System is not SISO"
-            return
-        p=exp(sys.poles*Ts)
-        z=exp(sys.zeros*Ts)
-        infinite_zeros = len(sys.poles) - len(sys.zeros) - 1
-        for i in range(0,infinite_zeros):
-            z=hstack((z,-1))
-        [A,B,C,D]=zpk2ss(z,p,1)
-        sysd=StateSpace(A,B,C,D,Ts)
-        cg = dcgain(sys)
-        dg = dcgain(sysd)
-        [A,B,C,D]=zpk2ss(z,p,cg/dg)
-    else:
-        print "Method not supported"
-        return
-    
-    sysd=StateSpace(A,B,C,D,Ts)
-    if flag==1:
-        sysd=ss2tf(sysd)
-    return sysd
+from matplotlib.pyplot import *
+from control import *
+from supsictrl import _wrapper
 
 def d2c(sys,method='zoh'):
     """Continous to discrete conversion with ZOH method
@@ -189,130 +107,93 @@ def d2c(sys,method='zoh'):
         sysc=ss2tf(sysc)
     return sysc
 
-def dsimul(sys,u):
-    """Simulate the discrete system sys
-    Only for discrete systems!!!
+def dlqr(*args, **keywords):
+    """Linear quadratic regulator design for discrete systems
 
-    Call:
-    y=dsimul(sys,u)
+    Usage
+    =====
+    [K, S, E] = dlqr(A, B, Q, R, [N])
+    [K, S, E] = dlqr(sys, Q, R, [N])
 
-    Parameters
-    ----------
-    sys : Discrete System in State Space form
-    u   : input vector
-    Returns
+    The dlqr() function computes the optimal state feedback controller
+    that minimizes the quadratic cost
+
+        J = \sum_0^\infty x' Q x + u' R u + 2 x' N u
+
+    Inputs
+    ------
+    A, B: 2-d arrays with dynamics and input matrices
+    sys: linear I/O system 
+    Q, R: 2-d array with state and input weight matrices
+    N: optional 2-d array with cross weight matrix
+
+    Outputs
     -------
-    y: ndarray
-    Simulation results
-
+    K: 2-d array with state feedback gains
+    S: 2-d array with solution to Riccati equation
+    E: 1-d array with eigenvalues of the closed loop system
     """
-    a=mat(sys.A)
-    b=mat(sys.B)
-    c=mat(sys.C)
-    d=mat(sys.D)
-    nx=shape(a)[0]
-    ns=shape(u)[1]
-    xk=zeros((nx,1))
-    for i in arange(0,ns):
-        uk=u[:,i]
-        xk_1=a*xk+b*uk
-        yk=c*xk+d*uk
-        xk=xk_1
-        if i==0:
-            y=yk
-        else:
-            y=hstack((y,yk))
-    y=array(y).T
-    return y
 
-def dstep(sys,Tf=10.0):
-    """Plot the step response of the discrete system sys
-    Only for discrete systems!!!
+    # 
+    # Process the arguments and figure out what inputs we received
+    #
+    
+    # Get the system description
+    if (len(args) < 3):
+        raise ControlArgument("not enough input arguments")
 
-    Call:
-    y=dstep(sys, [,Tf=final time]))
+    elif (ctrlutil.issys(args[0])):
+        # We were passed a system as the first argument; extract A and B
+        A = array(args[0].A, ndmin=2, dtype=float);
+        B = array(args[0].B, ndmin=2, dtype=float);
+        index = 1;
+        if args[0].dt==0.0:
+            print "dlqr works only for discrete systems!"
+            return
+    else:
+        # Arguments should be A and B matrices
+        A = array(args[0], ndmin=2, dtype=float);
+        B = array(args[1], ndmin=2, dtype=float);
+        index = 2;
 
-    Parameters
-    ----------
-    sys : Discrete System in State Space form
-    Tf  : Final simulation time
- 
-    Returns
-    -------
-    Nothing
+    # Get the weighting matrices (converting to matrices, if needed)
+    Q = array(args[index], ndmin=2, dtype=float);
+    R = array(args[index+1], ndmin=2, dtype=float);
+    if (len(args) > index + 2): 
+        N = array(args[index+2], ndmin=2, dtype=float);
+        Nflag = 1;
+    else:
+        N = zeros((Q.shape[0], R.shape[1]));
+        Nflag = 0;
 
-    """
-    Ts=sys.dt
-    if Ts==0.0:
-        "Only discrete systems allowed!"
-        return
+    # Check dimensions for consistency
+    nstates = B.shape[0];
+    ninputs = B.shape[1];
+    if (A.shape[0] != nstates or A.shape[1] != nstates):
+        raise ControlDimension("inconsistent system dimensions")
 
-    ns=int(Tf/Ts+1)
-    u=ones((1,ns))
-    y=dsimul(sys,u)
-    T=arange(0,Tf+Ts/2,Ts)
-    plot(T,y)
-    grid()
-    show()
+    elif (Q.shape[0] != nstates or Q.shape[1] != nstates or
+          R.shape[0] != ninputs or R.shape[1] != ninputs or
+          N.shape[0] != nstates or N.shape[1] != ninputs):
+        raise ControlDimension("incorrect weighting matrix dimensions")
 
-def dimpulse(sys,Tf=10.0):
-    """Plot the impulse response of the discrete system sys
-    Only for discrete systems!!!
+    if Nflag==1:
+        Ao=A-B*inv(R)*N.T
+        Qo=Q-N*inv(R)*N.T
+    else:
+        Ao=A
+        Qo=Q
+    
+    #Solve the riccati equation
+    (X,L,G) = dare(Ao,B,Qo,R)
+#    X = bb_dare(Ao,B,Qo,R)
 
-    Call:
-    y=dimpulse(sys,[,Tf=final time]))
-
-    Parameters
-    ----------
-    sys : Discrete System in State Space form
-    Tf  : Final simulation time
- 
-    Returns
-    -------
-    Nothing
-
-    """
-    Ts=sys.dt
-    if Ts==0.0:
-        "Only discrete systems allowed!"
-        return
-
-    ns=int(Tf/Ts+1)
-    u=zeros((1,ns))
-    u[0,0]=1/Ts
-    y=dsimul(sys,u)
-    T=arange(0,Tf+Ts/2,Ts)
-    plot(T,y)
-    grid()
-    show()
-
-# Step response (plot)
-def bb_step(sys,X0=None,Tf=None,Ts=0.001):
-    """Plot the step response of the continous system sys
-
-    Call:
-    y=bb_step(sys [,Tf=final time] [,Ts=time step])
-
-    Parameters
-    ----------
-    sys : Continous System in State Space form
-    X0: Initial state vector (not used yet)
-    Ts  : sympling time
-    Tf  : Final simulation time
- 
-    Returns
-    -------
-    Nothing
-
-    """
-    if Tf==None:
-        vals = eigvals(sys.A)
-        r = min(abs(real(vals)))
-        if r < 1e-10:
-            r = 0.1
-        Tf = 7.0 / r
-    sysd=c2d(sys,Ts)
-    dstep(sysd,Tf=Tf)
+    # Now compute the return value
+    Phi=mat(A)
+    H=mat(B)
+    K=inv(H.T*X*H+R)*(H.T*X*Phi+N.T)
+    L=eig(Phi-H*K)
+    return K,X,L
 
 def full_obs(sys,poles):
     """Full order observer of the system sys
@@ -338,7 +219,7 @@ def full_obs(sys,poles):
     b=mat(sys.B)
     c=mat(sys.C)
     d=mat(sys.D)
-    L=place(a.T,c.T,poles)
+    L=placep(a.T,c.T,poles)
     L=mat(L).T
     Ao=a-L*c
     Bo=hstack((b-L*d,L))
@@ -376,7 +257,6 @@ def red_obs(sys,T,poles):
     d=mat(sys.D)
     T=mat(T)
     P=mat(vstack((c,T)))
-    # poles=mat(poles)
     invP=inv(P)
     AA=P*a*invP
     ny=shape(c)[0]
@@ -388,7 +268,7 @@ def red_obs(sys,T,poles):
     A21=AA[ny:nx,0:ny]
     A22=AA[ny:nx,ny:nx]
 
-    L1=place(A22.T,A12.T,poles)
+    L1=placep(A22.T,A12.T,poles)
     L1=mat(L1).T
 
     nn=nx-ny
@@ -573,26 +453,78 @@ def set_aw(sys,poles):
     -------
     sys_in, sys_fbk: controller in input and feedback part
     """
-#   sys=StateSpace(sys);
-    sys=ss(sys);
+    sys = ss(sys)
     den_old=poly(eigvals(sys.A))
+    sys=tf(sys)
     den = poly(poles)
     tmp= tf(den_old,den,sys.dt)
-    tmpss=tf2ss(tmp)
-#   sys_in=StateSpace(tmp*sys)
-    sys_in=ss(tmp*sys)
-    sys_in.dt=sys.dt
-#    sys_fbk=StateSpace(1-tmp)
-    sys_fbk=ss(1-tmp)
-    sys_fbk.dt=sys.dt
+    sys_in=tmp*sys
+    sys_in = sys_in.minreal()
+    sys_in = ss(sys_in)
+    sys_fbk=1-tmp
+    sys_fbk = sys_fbk.minreal()
+    sys_fbk = ss(sys_fbk)
     return sys_in, sys_fbk
+
+def placep(A,B,P):
+    """Return the steady state value of the step response os sysmatrix K for
+    pole placement
+
+    Usage
+    =====
+    K = placep(A,B,P)
+
+    Inputs
+    ------
+
+    A  : State matrix A
+    B  : INput matrix
+    P  : desired poles
+
+    Outputs
+    -------
+    K : State gains for pole placement
+    """
+    
+    n = shape(A)[0]
+    m = shape(B)[1]
+    tol = 0.0
+    mode = 1;
+
+    wrka = zeros((n,m))
+    wrk1 = zeros(m)
+    wrk2 = zeros(m)
+    iwrk = zeros((m),np.int)
+
+    A,B,ncont,indcont,nblk,z = _wrapper.ssxmc(n,m,A,n,B,wrka,wrk1,wrk2,iwrk,tol,mode)
+    P = sort(P)
+    wr = real(P)
+    wi = imag(P)
+
+    g = zeros((m,n))
+
+    mx = max(2,m)
+    rm1 = zeros((m,m))
+    rm2 = zeros((m,mx))
+    rv1 = zeros(n)
+    rv2 = zeros(n)
+    rv3 = zeros(m)
+    rv4 = zeros(m)
+
+    A,B,g,z,ierr,jpvt = _wrapper.polmc(A,B,g,wr,wi,z,indcont,nblk,rm1, rm2, rv1, rv2, rv3, rv4)
+
+    return g
+
+"""
+These functions are now implemented in python control and should not be used anymore
+"""
 
 def bb_dare(A,B,Q,R):
     """Solve Riccati equation for discrete time systems
 
     Usage
     =====
-    [K, S, E] = care(A, B, Q, R)
+    [K, S, E] = bb_dare(A, B, Q, R)
 
     Inputs
     ------
@@ -621,196 +553,6 @@ def bb_dare(A,B,Q,R):
     return X
 
 
-def bb_dlqr(*args, **keywords):
-    """Linear quadratic regulator design for discrete systems
-
-    Usage
-    =====
-    [K, S, E] = dlqr(A, B, Q, R, [N])
-    [K, S, E] = dlqr(sys, Q, R, [N])
-
-    The dlqr() function computes the optimal state feedback controller
-    that minimizes the quadratic cost
-
-        J = \sum_0^\infty x' Q x + u' R u + 2 x' N u
-
-    Inputs
-    ------
-    A, B: 2-d arrays with dynamics and input matrices
-    sys: linear I/O system 
-    Q, R: 2-d array with state and input weight matrices
-    N: optional 2-d array with cross weight matrix
-
-    Outputs
-    -------
-    K: 2-d array with state feedback gains
-    S: 2-d array with solution to Riccati equation
-    E: 1-d array with eigenvalues of the closed loop system
-    """
-
-    # 
-    # Process the arguments and figure out what inputs we received
-    #
-    
-    # Get the system description
-    if (len(args) < 3):
-        raise ControlArgument("not enough input arguments")
-
-    elif (ctrlutil.issys(args[0])):
-        # We were passed a system as the first argument; extract A and B
-        A = array(args[0].A, ndmin=2, dtype=float);
-        B = array(args[0].B, ndmin=2, dtype=float);
-        index = 1;
-        if args[0].dt==0.0:
-            print "dlqr works only for discrete systems!"
-            return
-    else:
-        # Arguments should be A and B matrices
-        A = array(args[0], ndmin=2, dtype=float);
-        B = array(args[1], ndmin=2, dtype=float);
-        index = 2;
-
-    # Get the weighting matrices (converting to matrices, if needed)
-    Q = array(args[index], ndmin=2, dtype=float);
-    R = array(args[index+1], ndmin=2, dtype=float);
-    if (len(args) > index + 2): 
-        N = array(args[index+2], ndmin=2, dtype=float);
-        Nflag = 1;
-    else:
-        N = zeros((Q.shape[0], R.shape[1]));
-        Nflag = 0;
-
-    # Check dimensions for consistency
-    nstates = B.shape[0];
-    ninputs = B.shape[1];
-    if (A.shape[0] != nstates or A.shape[1] != nstates):
-        raise ControlDimension("inconsistent system dimensions")
-
-    elif (Q.shape[0] != nstates or Q.shape[1] != nstates or
-          R.shape[0] != ninputs or R.shape[1] != ninputs or
-          N.shape[0] != nstates or N.shape[1] != ninputs):
-        raise ControlDimension("incorrect weighting matrix dimensions")
-
-    if Nflag==1:
-        Ao=A-B*inv(R)*N.T
-        Qo=Q-N*inv(R)*N.T
-    else:
-        Ao=A
-        Qo=Q
-    
-    #Solve the riccati equation
-    # (X,L,G) = dare(Ao,B,Qo,R)
-    X = bb_dare(Ao,B,Qo,R)
-
-    # Now compute the return value
-    Phi=mat(A)
-    H=mat(B)
-    K=inv(H.T*X*H+R)*(H.T*X*Phi+N.T)
-    L=eig(Phi-H*K)
-    return K,X,L
-
-
-def dlqr(*args, **keywords):
-    """Linear quadratic regulator design
-
-    The lqr() function computes the optimal state feedback controller
-    that minimizes the quadratic cost
-
-    .. math:: J = \int_0^\infty x' Q x + u' R u + 2 x' N u
-
-    The function can be called with either 3, 4, or 5 arguments:
-    
-    * ``lqr(sys, Q, R)``
-    * ``lqr(sys, Q, R, N)``
-    * ``lqr(A, B, Q, R)``
-    * ``lqr(A, B, Q, R, N)``
-    
-    Parameters
-    ----------
-    A, B: 2-d array
-        Dynamics and input matrices
-    sys: Lti (StateSpace or TransferFunction)
-        Linear I/O system 
-    Q, R: 2-d array 
-        State and input weight matrices
-    N: 2-d array, optional  
-        Cross weight matrix
-
-    Returns
-    -------
-    K: 2-d array 
-        State feedback gains
-    S: 2-d array
-        Solution to Riccati equation
-    E: 1-d array 
-        Eigenvalues of the closed loop system
-    
-    Examples
-    --------
-    >>> K, S, E = lqr(sys, Q, R, [N])
-    >>> K, S, E = lqr(A, B, Q, R, [N])
-
-    """
-
-    # Make sure that SLICOT is installed
-    try:
-        from slycot import sb02md
-        from slycot import sb02mt
-    except ImportError:
-        raise ControlSlycot("can't find slycot module 'sb02md' or 'sb02nt'")
-
-    # 
-    # Process the arguments and figure out what inputs we received
-    #
-    
-    # Get the system description
-    if (len(args) < 4):
-        raise ControlArgument("not enough input arguments")
-
-    elif (ctrlutil.issys(args[0])):
-        # We were passed a system as the first argument; extract A and B
-        #! TODO: really just need to check for A and B attributes
-        A = np.array(args[0].A, ndmin=2, dtype=float);
-        B = np.array(args[0].B, ndmin=2, dtype=float);
-        index = 1;
-    else:
-        # Arguments should be A and B matrices
-        A = np.array(args[0], ndmin=2, dtype=float);
-        B = np.array(args[1], ndmin=2, dtype=float);
-        index = 2;
-
-    # Get the weighting matrices (converting to matrices, if needed)
-    Q = np.array(args[index], ndmin=2, dtype=float);
-    R = np.array(args[index+1], ndmin=2, dtype=float);
-    if (len(args) > index + 2): 
-        N = np.array(args[index+2], ndmin=2, dtype=float);
-    else:
-        N = np.zeros((Q.shape[0], R.shape[1]));
-
-    # Check dimensions for consistency
-    nstates = B.shape[0];
-    ninputs = B.shape[1];
-    if (A.shape[0] != nstates or A.shape[1] != nstates):
-        raise ControlDimension("inconsistent system dimensions")
-
-    elif (Q.shape[0] != nstates or Q.shape[1] != nstates or
-          R.shape[0] != ninputs or R.shape[1] != ninputs or
-          N.shape[0] != nstates or N.shape[1] != ninputs):
-        raise ControlDimension("incorrect weighting matrix dimensions")
-
-    # Compute the G matrix required by SB02MD
-    A_b,B_b,Q_b,R_b,L_b,ipiv,oufact,G = \
-        sb02mt(nstates, ninputs, B, R, A, Q, N, jobl='N');
-
-    # Call the SLICOT function
-    X,rcond,w,S,U,A_inv = sb02md(nstates, A_b, G, Q_b, 'D')
-
-    # Now compute the return value
-    K = np.dot(np.linalg.inv(R), (np.dot(B.T, X) + N.T));
-    S = X;
-    E = w[0:nstates];
-
-    return K, S, E
 
 def bb_dcgain(sys):
     """Return the steady state value of the step response os sys
@@ -842,3 +584,4 @@ def bb_dcgain(sys):
     else:
         gm=-c*inv(a)*b+d
     return array(gm)
+
