d0bfec769cf66b9f0ec52858bf30308b6e72ec32
Rory Yorke <rory.yorke@gmail.com>
2015-02-15 15:43:04 +0200

    Documentation fix: document R is None behaviour
    For both standard and generalized case, the behaviour of care() w.r.t. R
    is the same.
    
    Without this change, it's not clear that the care(A,B,Q,R) is a valid
    call form.

--- control/mateqn.py
+++ control/mateqn.py
@@ -411,25 +411,27 @@ def dlyap(A,Q,C=None,E=None):
 #### Riccati equation solvers care and dare
 
 def care(A,B,Q,R=None,S=None,E=None):
-    """ (X,L,G) = care(A,B,Q) solves the continuous-time algebraic Riccati
+    """ (X,L,G) = care(A,B,Q,R=None) solves the continuous-time algebraic Riccati
     equation
 
-        A^T X + X A - X B B^T X + Q = 0
+        A^T X + X A - X B R^-1 B^T X + Q = 0
 
-    where A and Q are square matrices of the same dimension. Further, Q
-    is a symmetric matrix. The function returns the solution X, the gain
-    matrix G = B^T X and the closed loop eigenvalues L, i.e., the eigenvalues
-    of A - B G.
+    where A and Q are square matrices of the same dimension. Further,
+    Q and R are a symmetric matrices. If R is None, it is set to the
+    identity matrix. The function returns the solution X, the gain
+    matrix G = B^T X and the closed loop eigenvalues L, i.e., the
+    eigenvalues of A - B G.
 
     (X,L,G) = care(A,B,Q,R,S,E) solves the generalized continuous-time
     algebraic Riccati equation
 
         A^T X E + E^T X A - (E^T X B + S) R^-1 (B^T X E + S^T) + Q = 0
 
-    where A, Q and E are square matrices of the same dimension. Further, Q and
-    R are symmetric matrices. The function returns the solution X, the gain
-    matrix G = R^-1 (B^T X E + S^T) and the closed loop eigenvalues L, i.e.,
-    the eigenvalues of A - B G , E. """
+    where A, Q and E are square matrices of the same
+    dimension. Further, Q and R are symmetric matrices. If R is None,
+    it is set to the identity matrix. The function returns the
+    solution X, the gain matrix G = R^-1 (B^T X E + S^T) and the
+    closed loop eigenvalues L, i.e., the eigenvalues of A - B G , E."""
 
     # Make sure we can import required slycot routine
     try:
