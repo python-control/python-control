6d337541fa6e48753105295d59f1b9abc2cc36d0
Clancy Rowley <cwrowley@princeton.edu>
2015-04-02 17:27:47 -0400

    Add conda recipe
    To build the conda package:
      conda build conda-recipe
    
    Note that the version is automatically set according to the files
    generated by make_version.py, which is automatically run by the recipe

--- make_version.py
+++ make_version.py
@@ -22,6 +22,19 @@ def main():
             fd.write('__version__ = "%s.post%s"\n' % (version, build))
         fd.write('__commit__ = "%s"\n' % (commit))
 
+    # Write files for conda version number
+    SRC_DIR = os.environ.get('SRC_DIR', '.')
+    conda_version_path = os.path.join(SRC_DIR, '__conda_version__.txt')
+    print("Writing %s" % conda_version_path)
+    with open(conda_version_path, 'w') as conda_version:
+        conda_version.write(version)
+
+    conda_buildnum_path = os.path.join(SRC_DIR, '__conda_buildnum__.txt')
+    print("Writing %s" % conda_buildnum_path)
+
+    with open(conda_buildnum_path, 'w') as conda_buildnum:
+        conda_buildnum.write(build)
+
 
 if __name__ == '__main__':
     main()
