9ded2bd4879dfba67b5730a6eef9557de6f212fc
Clancy Rowley <cwrowley@princeton.edu>
2015-04-21 14:57:59 -0400

    Fix whitespace in delay

--- control/delay.py
+++ control/delay.py
@@ -3,7 +3,7 @@
 #
 # Author: Sawyer Fuller
 # Date: 26 Aug 2010
-# 
+#
 # This file contains functions for implementing time delays (currently
 # only the pade() function).
 #
@@ -16,16 +16,16 @@
 #
 # 1. Redistributions of source code must retain the above copyright
 #    notice, this list of conditions and the following disclaimer.
-# 
+#
 # 2. Redistributions in binary form must reproduce the above copyright
 #    notice, this list of conditions and the following disclaimer in the
 #    documentation and/or other materials provided with the distribution.
-# 
+#
 # 3. Neither the name of the California Institute of Technology nor
 #    the names of its contributors may be used to endorse or promote
 #    products derived from this software without specific prior
 #    written permission.
-# 
+#
 # THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
 # "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
 # LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
@@ -38,30 +38,30 @@
 # OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
 # OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
 # SUCH DAMAGE.
-# 
+#
 # $Id$
 
 # Python 3 compatability (needs to go here)
 from __future__ import print_function
 
 def pade(T, n=1):
-    """ 
+    """
     Create a linear system that approximates a delay.
-    
+
     Return the numerator and denominator coefficients of the Pade approximation.
-    
+
     Parameters
     ----------
     T : number
         time delay
     n : integer
         order of approximation
-        
+
     Returns
-    -------       
+    -------
     num, den : array
         Polynomial coefficients of the delay model, in descending powers of s.
-    
+
     Notes
     -----
     Based on an algorithm in Golub and van Loan, "Matrix Computation" 3rd.
@@ -79,7 +79,7 @@ def pade(T, n=1):
         for k in range(1, n+1):
             c = T * c * (n - k + 1)/(2 * n - k + 1)/k
             num[n - k] = c * (-1)**k
-            den[n - k] = c 
+            den[n - k] = c
         num = [coeff/den[0] for coeff in num]
         den = [coeff/den[0] for coeff in den]
-    return num, den 
+    return num, den
