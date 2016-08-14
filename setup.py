58a3ff2036aab49b63d07684c60a35d59cb395f3
Scott C. Livingston <slivingston@cds.caltech.edu>
2016-08-13 20:15:48 -0400

    DOC: Declare support for Python 3.5 in setup.py classifiers

--- setup.py
+++ setup.py
@@ -21,6 +21,7 @@ Programming Language :: Python :: 2.7
 Programming Language :: Python :: 3
 Programming Language :: Python :: 3.3
 Programming Language :: Python :: 3.4
+Programming Language :: Python :: 3.5
 Topic :: Software Development
 Topic :: Scientific/Engineering
 Operating System :: Microsoft :: Windows
