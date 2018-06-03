9f13a778cf81c1a42c20b58d609921f5c90dd902
David de Jong <daviddejong@me.com>
2018-06-03 15:54:57 +0200

    small change to sisotool test

--- control/tests/sisotool_test.py
+++ control/tests/sisotool_test.py
@@ -14,7 +14,7 @@ class TestSisotool(unittest.TestCase):
         self.system = TransferFunction([1000],[1,25,100,0])
 
     def test_sisotool(self):
-        sisotool(self.system)
+        sisotool(self.system,Hz=False)
         fig = plt.gcf()
         ax_mag,ax_rlocus,ax_phase,ax_step = fig.axes[0],fig.axes[1],fig.axes[2],fig.axes[3]
 
@@ -31,7 +31,6 @@ class TestSisotool(unittest.TestCase):
         assert_array_almost_equal(ax_mag.lines[0].get_data()[1][10:20],bode_mag_original)
 
         # Check the step response before moving the point
-        print(ax_step.lines[0].get_data()[1][:10])
         step_response_original = np.array([ 0.,          0.02233651,  0.13118374,  0.33078542,  0.5907113,   0.87041549, 1.13038536,  1.33851053,  1.47374666,  1.52757114])
         assert_array_almost_equal(ax_step.lines[0].get_data()[1][:10],step_response_original)
 
@@ -55,8 +54,6 @@ class TestSisotool(unittest.TestCase):
         moved_point_0 = (np.array([-29.91742755]), np.array([0.]))
         moved_point_1 = (np.array([2.45871378]), np.array([-15.52647768]))
         moved_point_2 = (np.array([2.45871378]), np.array([15.52647768]))
-        for line in ax_rlocus.lines:
-            print(line.get_data())
         assert_array_almost_equal(ax_rlocus.lines[-3].get_data(),moved_point_0)
         assert_array_almost_equal(ax_rlocus.lines[-2].get_data(),moved_point_1)
         assert_array_almost_equal(ax_rlocus.lines[-1].get_data(),moved_point_2)
