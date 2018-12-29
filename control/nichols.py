f4e3a1cf690680bfe459bd99370d3b801be33bce
Anthony De Bortoli <anthony.debortoli@protonmail.com>
2018-12-29 12:00:31 +0100

    Add docstring module to nichols.py

--- control/nichols.py
+++ control/nichols.py
@@ -1,3 +1,13 @@
+"""nichols.py
+
+Functions for plotting Black-Nichols charts.
+
+Routines in this module:
+
+nichols.nichols_plot aliased as nichols.nichols
+nichols.nichols_grid
+"""
+
 # nichols.py - Nichols plot
 #
 # Contributed by Allan McInnes <Allan.McInnes@canterbury.ac.nz>
