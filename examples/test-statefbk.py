d478935268fc6c92b67462df4197f2d617eb5568
Anthony De Bortoli <anthony.debortoli@protonmail.com>
2019-01-04 23:50:06 +0100

    [test-statefdk.py] Refactor style and imports

--- examples/test-statefbk.py
+++ examples/test-statefbk.py
@@ -1,26 +1,33 @@
-# test-statefbk.py - Unit tests for state feedback code
+# test-statefbk.py - Example for state feedback code
 # RMM, 6 Sep 2010
 
-import numpy as np              # Numerical library
-from scipy import *             # Load the scipy functions
-from control.matlab import *    # Load the controls systems library
+from scipy import *  # Load the scipy functions
+from control.matlab import *  # Load the controls systems library
 
 # Parameters defining the system
-m = 250.0			# system mass
-k = 40.0			# spring constant
-b = 60.0			# damping constant
+
+m = 250.0  # system mass
+k = 40.0  # spring constant
+b = 60.0  # damping constant
 
 # System matrices
-A = matrix([[1, -1, 1.], [1, -k/m, -b/m], [1, 1, 1]])
-B = matrix([[0], [1/m], [1]])
-C = matrix([[1., 0, 1.]])
-sys = ss(A, B, C, 0);
+A = matrix([[1, -1, 1.],
+            [1, -k / m, -b / m],
+            [1, 1, 1]])
 
-# Controllability
+B = matrix([[0],
+            [1 / m],
+            [1]])
+
+C = matrix([[1., 0, 1.]])
+
+sys = ss(A, B, C, 0)
+
+# Check controllability
 Wc = ctrb(A, B)
 print "Wc = ", Wc
 
-# Observability
+# Check Observability
 Wo = obsv(A, C)
 print "Wo = ", Wo
 
