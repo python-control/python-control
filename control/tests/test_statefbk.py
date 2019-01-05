e26f9cca206b0d1c6f63f0ebc651459acd4ecf1e
Anthony De Bortoli <anthony.debortoli@protonmail.com>
2019-01-05 18:51:55 +0100

    [test_statefbk.py] Clean style and remove matrix usage

--- control/tests/test_statefbk.py
+++ control/tests/test_statefbk.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 #
-# statefbk_test.py - test state feedback functions
+# test_statefbk.py - test state feedback functions
 # RMM, 30 Mar 2011 (based on TestStatefbk from v0.4a)
 
 from __future__ import print_function
@@ -25,118 +25,117 @@ class TestStatefbk(unittest.TestCase):
         # get consistent test results
         np.random.seed(0)
 
-    def testCtrbSISO(self):
-        A = np.matrix("1. 2.; 3. 4.")
-        B = np.matrix("5.; 7.")
-        Wctrue = np.matrix("5. 19.; 7. 43.")
-        Wc = ctrb(A, B)
+        # 2 states SISO system
+        self.A_siso = np.array([[1., -2.],
+                                [3., -4.]])
+        self.B_siso = np.array([[5.], [7.]])
+        self.C_siso = np.array([6., 8.])
+        self.D_siso = np.array([9.])
+        self.sys_siso = ss(self.A_siso, self.B_siso, self.C_siso, self.D_siso)
+
+        # 2 states MIMO (2 inputs, 2 outputs) system
+        self.A_mimo = np.array([[1., -2.],
+                                [3., -4.]])
+        self.B_mimo = np.array([[5., 6.],
+                                [7., 8.]])
+        self.C_mimo = np.array([[4., 5.],
+                                [6., 7.]])
+        self.D_mimo = np.array([[13., 14.],
+                                [15., 16.]])
+        self.sys_mimo = ss(self.A_mimo, self.B_mimo, self.C_mimo, self.D_mimo)
+
+    def test_ctrb_siso(self):
+        Wctrue = np.array([[5., -9.],
+                           [7., -13.]])
+        Wc = ctrb(self.A_siso, self.B_siso)
         np.testing.assert_array_almost_equal(Wc, Wctrue)
 
-    def testCtrbMIMO(self):
-        A = np.matrix("1. 2.; 3. 4.")
-        B = np.matrix("5. 6.; 7. 8.")
-        Wctrue = np.matrix("5. 6. 19. 22.; 7. 8. 43. 50.")
-        Wc = ctrb(A, B)
+    def test_ctrb_mimo(self):
+        Wctrue = np.array([[5., 6., -9., -10.],
+                           [7., 8., -13., -14.]])
+        Wc = ctrb(self.A_mimo, self.B_mimo)
         np.testing.assert_array_almost_equal(Wc, Wctrue)
 
-    def testObsvSISO(self):
-        A = np.matrix("1. 2.; 3. 4.")
-        C = np.matrix("5. 7.")
-        Wotrue = np.matrix("5. 7.; 26. 38.")
-        Wo = obsv(A, C)
+    def test_obsv_siso(self):
+        Wotrue = np.array([[6., 8.],
+                           [30., -44.]])
+        Wo = obsv(self.A_siso, self.C_siso)
         np.testing.assert_array_almost_equal(Wo, Wotrue)
 
-    def testObsvMIMO(self):
-        A = np.matrix("1. 2.; 3. 4.")
-        C = np.matrix("5. 6.; 7. 8.")
-        Wotrue = np.matrix("5. 6.; 7. 8.; 23. 34.; 31. 46.")
-        Wo = obsv(A, C)
+    def test_obsv_mimo(self):
+        Wotrue = np.array([[4., 5.],
+                           [6., 7.],
+                           [19., -28.],
+                           [27., -40.]])
+        Wo = obsv(self.A_mimo, self.C_mimo)
         np.testing.assert_array_almost_equal(Wo, Wotrue)
 
-    def testCtrbObsvDuality(self):
-        A = np.matrix("1.2 -2.3; 3.4 -4.5")
-        B = np.matrix("5.8 6.9; 8. 9.1")
-        Wc = ctrb(A, B);
+    def test_ctrb_obsv_duality(self):
+        A = np.array([[1.2, -2.3],
+                      [3.4, -4.5]])
+        B = np.array([[5.8, 6.9],
+                      [8., 9.1]])
+        Wc = ctrb(A, B)
         A = np.transpose(A)
         C = np.transpose(B)
-        Wo = np.transpose(obsv(A, C));
+        Wo = np.transpose(obsv(A, C))
         np.testing.assert_array_almost_equal(Wc, Wo)
 
     @unittest.skipIf(not slycot_check(), "slycot not installed")
-    def testGramWc(self):
-        A = np.matrix("1. -2.; 3. -4.")
-        B = np.matrix("5. 6.; 7. 8.")
-        C = np.matrix("4. 5.; 6. 7.")
-        D = np.matrix("13. 14.; 15. 16.")
-        sys = ss(A, B, C, D)
-        Wctrue = np.matrix("18.5 24.5; 24.5 32.5")
-        Wc = gram(sys, 'c')
+    def test_gram_wc(self):
+        Wctrue = np.array([[18.5, 24.5],
+                           [24.5, 32.5]])
+        Wc = gram(self.sys_mimo, 'c')
         np.testing.assert_array_almost_equal(Wc, Wctrue)
 
     @unittest.skipIf(not slycot_check(), "slycot not installed")
-    def testGramRc(self):
-        A = np.matrix("1. -2.; 3. -4.")
-        B = np.matrix("5. 6.; 7. 8.")
-        C = np.matrix("4. 5.; 6. 7.")
-        D = np.matrix("13. 14.; 15. 16.")
-        sys = ss(A, B, C, D)
-        Rctrue = np.matrix("4.30116263 5.6961343; 0. 0.23249528")
-        Rc = gram(sys, 'cf')
+    def test_gram_rc(self):
+        Rctrue = np.array([[4.30116263, 5.6961343],
+                           [0., 0.23249528]])
+        Rc = gram(self.sys_mimo, 'cf')
         np.testing.assert_array_almost_equal(Rc, Rctrue)
 
     @unittest.skipIf(not slycot_check(), "slycot not installed")
-    def testGramWo(self):
-        A = np.matrix("1. -2.; 3. -4.")
-        B = np.matrix("5. 6.; 7. 8.")
-        C = np.matrix("4. 5.; 6. 7.")
-        D = np.matrix("13. 14.; 15. 16.")
-        sys = ss(A, B, C, D)
-        Wotrue = np.matrix("257.5 -94.5; -94.5 56.5")
-        Wo = gram(sys, 'o')
+    def test_gram_wo(self):
+        Wotrue = np.array([[257.5, -94.5],
+                           [-94.5, 56.5]])
+        Wo = gram(self.sys_mimo, 'o')
         np.testing.assert_array_almost_equal(Wo, Wotrue)
 
     @unittest.skipIf(not slycot_check(), "slycot not installed")
-    def testGramWo2(self):
-        A = np.matrix("1. -2.; 3. -4.")
-        B = np.matrix("5.; 7.")
-        C = np.matrix("6. 8.")
-        D = np.matrix("9.")
-        sys = ss(A, B, C, D)
-        Wotrue = np.matrix("198. -72.; -72. 44.")
-        Wo = gram(sys, 'o')
+    def test_gram_wo2(self):
+        Wotrue = np.array([[198., -72.],
+                           [-72., 44.]])
+        Wo = gram(self.sys_siso, 'o')
         np.testing.assert_array_almost_equal(Wo, Wotrue)
 
     @unittest.skipIf(not slycot_check(), "slycot not installed")
-    def testGramRo(self):
-        A = np.matrix("1. -2.; 3. -4.")
-        B = np.matrix("5. 6.; 7. 8.")
-        C = np.matrix("4. 5.; 6. 7.")
-        D = np.matrix("13. 14.; 15. 16.")
-        sys = ss(A, B, C, D)
-        Rotrue = np.matrix("16.04680654 -5.8890222; 0. 4.67112593")
-        Ro = gram(sys, 'of')
+    def test_gram_ro(self):
+        Rotrue = np.array([[16.04680654, -5.8890222],
+                           [0., 4.67112593]])
+        Ro = gram(self.sys_mimo, 'of')
         np.testing.assert_array_almost_equal(Ro, Rotrue)
 
-    def testGramsys(self):
+    def test_gram_sys(self):
         num = [1.]
         den = [1., 1., 1.]
         sys = tf(num, den)
         self.assertRaises(ValueError, gram, sys, 'o')
         self.assertRaises(ValueError, gram, sys, 'c')
 
-    def testAcker(self):
+    def test_acker(self):
         for states in range(1, self.maxStates):
             for i in range(self.maxTries):
                 # start with a random SS system and transform to TF then
                 # back to SS, check that the matrices are the same.
                 sys = rss(states, 1, 1)
-                if (self.debug):
+                if self.debug:
                     print(sys)
 
                 # Make sure the system is not degenerate
                 Cmat = ctrb(sys.A, sys.B)
                 if np.linalg.matrix_rank(Cmat) != states:
-                    if (self.debug):
+                    if self.debug:
                         print("  skipping (not reachable or ill conditioned)")
                         continue
 
@@ -159,7 +158,7 @@ class TestStatefbk(unittest.TestCase):
                 np.testing.assert_array_almost_equal(np.sort(poles),
                                                      np.sort(placed), decimal=4)
 
-    def testPlace(self):
+    def test_place(self):
         # Matrices shamelessly stolen from scipy example code.
         A = np.array([[1.380, -0.2077, 6.715, -5.676],
                       [-0.5814, -4.290, 0, 0.6750],
@@ -189,12 +188,12 @@ class TestStatefbk(unittest.TestCase):
         # Error not raised anymore as fallback solution is implemented.
         # np.testing.assert_raises(ValueError, place, A, B, P_repeated)
 
-    def testPlace_varga_continuous(self):
+    def test_place_varga_continuous(self):
         """
         Check that we can place eigenvalues for dtime=False
         """
-        A = np.array([[1., -2.], [3., -4.]])
-        B = np.array([[5.], [7.]])
+        A = self.A_siso
+        B = self.B_siso
 
         P = np.array([-2., -2.])
         K = _place_varga(A, B, P)
@@ -221,15 +220,15 @@ class TestStatefbk(unittest.TestCase):
         P_placed.sort()
         np.testing.assert_array_almost_equal(P, P_placed)
 
-    def testPlace_varga_continuous_partial_eigs(self):
+    def test_pleace_varga_continuous_partial_eigs(self):
         """
         Check that we are able to use the alpha parameter to only place
         a subset of the eigenvalues, for the continous time case.
         """
         # A matrix has eigenvalues at s=-1, and s=-2. Choose alpha = -1.5
         # and check that eigenvalue at s=-2 stays put.
-        A = np.array([[1., -2.], [3., -4.]])
-        B = np.array([[5.], [7.]])
+        A = self.A_siso
+        B = self.B_siso
 
         P = np.array([-3.])
         P_expected = np.array([-2.0, -3.0])
@@ -242,7 +241,7 @@ class TestStatefbk(unittest.TestCase):
         P_placed.sort()
         np.testing.assert_array_almost_equal(P_expected, P_placed)
 
-    def testPlace_varga_discrete(self):
+    def test_place_varga_discrete(self):
         """
         Check that we can place poles using dtime=True (discrete time)
         """
@@ -257,7 +256,7 @@ class TestStatefbk(unittest.TestCase):
         P_placed.sort()
         np.testing.assert_array_almost_equal(P, P_placed)
 
-    def testPlace_varga_discrete_partial_eigs(self):
+    def test_place_varga_discrete_partial_eigs(self):
         """"
         Check that we can only assign a single eigenvalue in the discrete
         time case.
@@ -275,7 +274,7 @@ class TestStatefbk(unittest.TestCase):
         P_placed.sort()
         np.testing.assert_array_almost_equal(P_expected, P_placed)
 
-    def check_LQR(self, K, S, poles, Q, R):
+    def check_lqr(self, K, S, poles, Q, R):
         S_expected = np.array(np.sqrt(Q * R))
         K_expected = S_expected / R
         poles_expected = np.array([-K_expected])
@@ -284,17 +283,17 @@ class TestStatefbk(unittest.TestCase):
         np.testing.assert_array_almost_equal(poles, poles_expected)
 
     @unittest.skipIf(not slycot_check(), "slycot not installed")
-    def test_LQR_integrator(self):
+    def test_lqr_integrator(self):
         A, B, Q, R = 0., 1., 10., 2.
         K, S, poles = lqr(A, B, Q, R)
-        self.check_LQR(K, S, poles, Q, R)
+        self.check_lqr(K, S, poles, Q, R)
 
     @unittest.skipIf(not slycot_check(), "slycot not installed")
-    def test_LQR_3args(self):
+    def test_lqr_3args(self):
         sys = ss(0., 1., 1., 0.)
         Q, R = 10., 2.
         K, S, poles = lqr(sys, Q, R)
-        self.check_LQR(K, S, poles, Q, R)
+        self.check_lqr(K, S, poles, Q, R)
 
     @unittest.skipIf(not slycot_check(), "slycot not installed")
     def test_care(self):
