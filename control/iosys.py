15a442a7d8c6bd8b49f1bb73731e181530171c46
Samuel Laferriere <samlaf92@gmail.com>
2020-05-18 11:09:40 -0400

    Update control/iosys.py
    Co-authored-by: Ben Greiner <code@bnavigator.de>

--- control/iosys.py
+++ control/iosys.py
@@ -918,7 +918,6 @@ class InterconnectedSystem(InputOutputSystem):
             # TODO: look for duplicated system names
             self.syslist_index[sys.name] = system_count
             system_count += 1
-            
         # Check for duplicate systems or duplicate names
         sysobj_list = []
         sysname_list = []
