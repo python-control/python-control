9f021192d670a789041ab4fa40941d84be787da5
Sawyer Fuller <minster@uw.edu>
2020-06-19 15:20:55 -0700

    adjusted sisotool test so tests pass with new default step response time window

--- control/tests/sisotool_test.py
+++ control/tests/sisotool_test.py
@@ -27,7 +27,7 @@ class TestSisotool(unittest.TestCase):
         assert_array_almost_equal(ax_rlocus.lines[2].get_data(),initial_point_2)
 
         # Check the step response before moving the point
-        step_response_original = np.array([ 0.,          0.02233651,  0.13118374,  0.33078542,  0.5907113,   0.87041549, 1.13038536,  1.33851053,  1.47374666,  1.52757114])
+        step_response_original = np.array([0.    , 0.0217, 0.1281, 0.3237, 0.5797, 0.8566, 1.116 , 1.3261, 1.4659, 1.526 ])
         assert_array_almost_equal(ax_step.lines[0].get_data()[1][:10],step_response_original)
 
         bode_plot_params = {
@@ -59,7 +59,7 @@ class TestSisotool(unittest.TestCase):
         assert_array_almost_equal(ax_mag.lines[0].get_data()[1][10:20],bode_mag_moved)
 
         # Check if the step response has changed
-        step_response_moved = np.array([[ 0.,          0.02458187,  0.16529784 , 0.46602716 , 0.91012035 , 1.43364313, 1.93996334 , 2.3190105  , 2.47041552 , 2.32724853] ])
+        step_response_moved = np.array([0.    , 0.0239, 0.161 , 0.4547, 0.8903, 1.407 , 1.9121, 2.2989, 2.4686, 2.353 ])
         assert_array_almost_equal(ax_step.lines[0].get_data()[1][:10],step_response_moved)
 
 
