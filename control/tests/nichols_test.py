3eb0a9260d4bbb17bd70661bf9f3e15f027f1a2b
bnavigator <code@bnavigator.de>
2020-07-26 21:00:51 +0200

    pytestify nichols_test

--- control/tests/nichols_test.py
+++ control/tests/nichols_test.py
@@ -1,34 +1,28 @@
-#!/usr/bin/env python
-#
-# nichols_test.py - test Nichols plot
-# RMM, 31 Mar 2011
+"""nichols_test.py - test Nichols plot
 
-import unittest
-import numpy as np
-from control.matlab import *
+RMM, 31 Mar 2011
+"""
 
-class TestStateSpace(unittest.TestCase):
-    """Tests for the Nichols plots."""
+import pytest
 
-    def setUp(self):
-        """Set up a system to test operations on."""
-
-        A = [[-3., 4., 2.], [-1., -3., 0.], [2., 5., 3.]]
-        B = [[1.], [-3.], [-2.]]
-        C = [[4., 2., -3.]]
-        D = [[0.]]
-
-        self.sys = StateSpace(A, B, C, D)
-
-    def testNicholsPlain(self):
-        """Generate a Nichols plot."""
-        nichols(self.sys)
-
-    def testNgrid(self):
-        """Generate a Nichols plot."""
-        nichols(self.sys, grid=False)
-        ngrid()
+from control import StateSpace, nichols_plot, nichols
 
 
-if __name__ == "__main__":
-    unittest.main()
+@pytest.fixture()
+def tsys():
+    """Set up a system to test operations on."""
+    A = [[-3., 4., 2.], [-1., -3., 0.], [2., 5., 3.]]
+    B = [[1.], [-3.], [-2.]]
+    C = [[4., 2., -3.]]
+    D = [[0.]]
+    return StateSpace(A, B, C, D)
+
+
+def test_nichols(tsys, mplcleanup):
+    """Generate a Nichols plot."""
+    nichols_plot(tsys)
+
+
+def test_nichols_alias(tsys, mplcleanup):
+    """Test the control.nichols alias and the grid=False parameter"""
+    nichols(tsys, grid=False)
