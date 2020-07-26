caed7128e0ebfe86448e2dfc42f93b9a46870925
bnavigator <code@bnavigator.de>
2020-07-26 14:42:38 +0200

    pytestify input_element_int_test

--- control/tests/input_element_int_test.py
+++ control/tests/input_element_int_test.py
@@ -1,54 +1,66 @@
-# input_element_int_test.py
-#
-# Author: Kangwon Lee (kangwonlee)
-# Date: 22 Oct 2017
-#
-# Unit tests contributed as part of PR #158, "SISO tf() may not work
-# with numpy arrays with numpy.int elements"
-#
-# Modified:
-# * 29 Dec 2017, RMM - updated file name and added header
+"""input_element_int_test.py
 
-import unittest
+Author: Kangwon Lee (kangwonlee)
+Date: 22 Oct 2017
+
+Modified:
+* 29 Dec 2017, RMM - updated file name and added header
+"""
+
 import numpy as np
-import control as ctl
+from control import dcgain, ss, tf
 
-class TestTfInputIntElement(unittest.TestCase):
-    # currently these do not pass
+class TestTfInputIntElement:
+    """input_element_int_test
+
+    Unit tests contributed as part of PR gh-158, "SISO tf() may not work
+    with numpy arrays with numpy.int elements
+    """
+
     def test_tf_den_with_numpy_int_element(self):
         num = 1
         den = np.convolve([1, 2, 1], [1, 1, 1])
 
-        sys = ctl.tf(num, den)
+        sys = tf(num, den)
 
-        self.assertAlmostEqual(1.0, ctl.dcgain(sys))
+        np.testing.assert_array_max_ulp(1., dcgain(sys))
 
     def test_tf_num_with_numpy_int_element(self):
         num = np.convolve([1], [1, 1])
         den = np.convolve([1, 2, 1], [1, 1, 1])
 
-        sys = ctl.tf(num, den)
+        sys = tf(num, den)
 
-        self.assertAlmostEqual(1.0, ctl.dcgain(sys))
+        np.testing.assert_array_max_ulp(1., dcgain(sys))
 
     # currently these pass
-    def test_tf_input_with_int_element_works(self):
+    def test_tf_input_with_int_element(self):
         num = 1
         den = np.convolve([1.0, 2, 1], [1, 1, 1])
 
-        sys = ctl.tf(num, den)
+        sys = tf(num, den)
 
-        self.assertAlmostEqual(1.0, ctl.dcgain(sys))
+        np.testing.assert_array_max_ulp(1., dcgain(sys))
 
     def test_ss_input_with_int_element(self):
-        ident = np.matrix(np.identity(2), dtype=int)
-        a = np.matrix([[0, 1],
-                       [-1, -2]], dtype=int) * ident
-        b = np.matrix([[0],
+        a = np.array([[0, 1],
+                      [-1, -2]], dtype=int)
+        b = np.array([[0],
                        [1]], dtype=int)
-        c = np.matrix([[0, 1]], dtype=int)
-        d = 0
+        c = np.array([[0, 1]], dtype=int)
+        d = np.array([[1]], dtype=int)
 
-        sys = ctl.ss(a, b, c, d)
-        sys2 = ctl.ss2tf(sys)
-        self.assertAlmostEqual(ctl.dcgain(sys), ctl.dcgain(sys2))
+        sys = ss(a, b, c, d)
+        sys2 = tf(sys)
+        np.testing.assert_array_max_ulp(dcgain(sys), dcgain(sys2))
+
+    def test_ss_input_with_0int_dcgain(self):
+        a = np.array([[0, 1],
+                      [-1, -2]], dtype=int)
+        b = np.array([[0],
+                       [1]], dtype=int)
+        c = np.array([[0, 1]], dtype=int)
+        d = 0
+        sys = ss(a, b, c, d)
+        np.testing.assert_allclose(dcgain(sys), 0,
+                                   atol=np.finfo(np.float).epsneg)
