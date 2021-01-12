36ecea1a94961e4d2c6e8c1fbb9fc119c91360ed
Richard Murray <murray@cds.caltech.edu>
2021-01-11 19:33:01 -0800

    update xvfb configuration; remove X11 marks

--- control/tests/conftest.py
+++ control/tests/conftest.py
@@ -26,8 +26,7 @@ matrixfilter = pytest.mark.filterwarnings("ignore:.*matrix subclass:"
                                           "PendingDeprecationWarning")
 matrixerrorfilter = pytest.mark.filterwarnings("error:.*matrix subclass:"
                                                "PendingDeprecationWarning")
-X11only = pytest.mark.skipif(os.getenv("DISPLAY") is None,
-                             reason="requires X11")
+
 
 @pytest.fixture(scope="session", autouse=True)
 def control_defaults():
