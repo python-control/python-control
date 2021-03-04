fixed bug in calculation of pct overshoot that neglects not strictly proper transfer functions.

control/tests/sisotool_test.py
@@ -67,12 +67,9 @@ class TestSisotool:
                                   initial_point_2, 4)
 
         # Check the step response before moving the point
-        # new array needed because change in compute step response default time
         step_response_original = np.array(
-            [0.    , 0.0069, 0.0448, 0.124 , 0.2427, 0.3933, 0.5653, 0.7473,
-             0.928 , 1.0969])
-        #old: np.array([0., 0.0217, 0.1281, 0.3237, 0.5797, 0.8566, 1.116,
-            # 1.3261, 1.4659, 1.526])
+            [0.    , 0.0366, 0.2032, 0.4857, 0.82  , 1.1358, 1.3762, 1.507 ,
+             1.5206, 1.4326])
         assert_array_almost_equal(
             ax_step.lines[0].get_data()[1][:10], step_response_original, 4)
 
@@ -115,10 +112,9 @@ class TestSisotool:
                                   bode_mag_moved, 4)
 
         # Check if the step response has changed
-        # new array needed because change in compute step response default time
         step_response_moved = np.array(
-            [0., 0.0072, 0.0516, 0.1554, 0.3281, 0.5681, 0.8646, 1.1987,
-             1.5452, 1.875])
+            [0.    , 0.0415, 0.2687, 0.7248, 1.3367, 1.9505, 2.3765, 2.4469,
+             2.0738, 1.2926])
         assert_array_almost_equal(
             ax_step.lines[0].get_data()[1][:10], step_response_moved, 4)
 

control/timeresp.py
@@ -814,7 +814,7 @@ def step_info(sys, T=None, T_num=None, SettlingTimeThreshold=0.02,
         'SettlingTime': SettlingTime,
         'SettlingMin': yout[tr_upper_index:].min(),
         'SettlingMax': yout.max(),
-        'Overshoot': 100. * (yout.max() - InfValue) / (InfValue - yout[0]),
+        'Overshoot': 100. * (yout.max() - InfValue) / InfValue,
         'Undershoot': yout.min(), # not very confident about this
         'Peak': yout[PeakIndex],
         'PeakTime':  T[PeakIndex],
@@ -1124,7 +1124,7 @@ def _ideal_tfinal_and_dt(sys, is_step=True):
     default_dt = 0.1
     total_cycles = 5  # number of cycles for oscillating modes
     pts_per_cycle = 25  # Number of points divide a period of oscillation
-    log_decay_percent = np.log(100)  # Factor of reduction for real pole decays
+    log_decay_percent = np.log(5000)  # Factor of reduction for real pole decays
 
     if sys._isstatic():
         tfinal = default_tfinal