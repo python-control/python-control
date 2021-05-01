e0dab934ce364fd4d59cac9ae9964f8ad054bee3
Ben Greiner <code@bnavigator.de>
2021-05-01 15:15:42 +0200

    use collections.UserDict for DefaultDict

--- control/config.py
+++ control/config.py
@@ -7,6 +7,8 @@
 # files.  For now, you can just choose between MATLAB and FBS default
 # values + tweak a few other things.
 
+
+import collections
 import warnings
 
 __all__ = ['defaults', 'set_defaults', 'reset_defaults',
@@ -22,7 +24,14 @@ _control_defaults = {
 }
 
 
-class DefaultDict(dict):
+class DefaultDict(collections.UserDict):
+    """Map names for settings from older version to their renamed ones.
+
+    If a user wants to write to an old setting, issue a warning and write to
+    the renamed setting instead. Accessing the old setting returns the value
+    from the new name.
+    """
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
@@ -30,24 +39,20 @@ class DefaultDict(dict):
         super().__setitem__(self._check_deprecation(key), value)
 
     def __missing__(self, key):
+        # An old key should never have been set. If it is being accessed
+        # through __getitem__, return the value from the new name.
         repl = self._check_deprecation(key)
         if self.__contains__(repl):
             return self[repl]
         else:
-            raise KeyError
-
-    def copy(self):
-        return DefaultDict(self)
-
-    def get(self, key, default=None):
-        return super().get(self._check_deprecation(key), default)
+            raise KeyError(key)
 
     def _check_deprecation(self, key):
         if self.__contains__(f"deprecated.{key}"):
             repl = self[f"deprecated.{key}"]
             warnings.warn(f"config.defaults['{key}'] has been renamed to "
                           f"config.defaults['{repl}'].",
-                          DeprecationWarning)
+                          FutureWarning, stacklevel=3)
             return repl
         else:
             return key
--- control/tests/config_test.py
+++ control/tests/config_test.py
@@ -50,38 +50,44 @@ class TestConfig:
         assert ct.config._get_param('config', 'test4', {'test4': 1}, None) == 1
 
     def test_default_deprecation(self):
-        ct.config.defaults['config.newkey'] = 1
         ct.config.defaults['deprecated.config.oldkey'] = 'config.newkey'
         ct.config.defaults['deprecated.config.oldmiss'] = 'config.newmiss'
 
         msgpattern = r'config\.oldkey.* has been renamed to .*config\.newkey'
 
-        with pytest.warns(DeprecationWarning, match=msgpattern):
+        ct.config.defaults['config.newkey'] = 1
+        with pytest.warns(FutureWarning, match=msgpattern):
             assert ct.config.defaults['config.oldkey'] == 1
-        with pytest.warns(DeprecationWarning, match=msgpattern):
+        with pytest.warns(FutureWarning, match=msgpattern):
             ct.config.defaults['config.oldkey'] = 2
-        with pytest.warns(DeprecationWarning, match=msgpattern):
+        with pytest.warns(FutureWarning, match=msgpattern):
             assert ct.config.defaults['config.oldkey'] == 2
         assert ct.config.defaults['config.newkey'] == 2
 
         ct.config.set_defaults('config', newkey=3)
-        with pytest.warns(DeprecationWarning, match=msgpattern):
+        with pytest.warns(FutureWarning, match=msgpattern):
             assert ct.config._get_param('config', 'oldkey') == 3
-        with pytest.warns(DeprecationWarning, match=msgpattern):
+        with pytest.warns(FutureWarning, match=msgpattern):
             ct.config.set_defaults('config', oldkey=4)
-        with pytest.warns(DeprecationWarning, match=msgpattern):
+        with pytest.warns(FutureWarning, match=msgpattern):
             assert ct.config.defaults['config.oldkey'] == 4
         assert ct.config.defaults['config.newkey'] == 4
 
+        ct.config.defaults.update({'config.newkey': 5})
+        with pytest.warns(FutureWarning, match=msgpattern):
+            ct.config.defaults.update({'config.oldkey': 6})
+        with pytest.warns(FutureWarning, match=msgpattern):
+            assert ct.config.defaults.get('config.oldkey') == 6
+
         with pytest.raises(KeyError):
-            with pytest.warns(DeprecationWarning, match=msgpattern):
+            with pytest.warns(FutureWarning, match=msgpattern):
                 ct.config.defaults['config.oldmiss']
         with pytest.raises(KeyError):
             ct.config.defaults['config.neverdefined']
 
         # assert that reset defaults keeps the custom type
         ct.config.reset_defaults()
-        with pytest.warns(DeprecationWarning,
+        with pytest.warns(FutureWarning,
                           match='bode.* has been renamed to.*freqplot'):
             assert ct.config.defaults['bode.Hz'] \
                 == ct.config.defaults['freqplot.Hz']
