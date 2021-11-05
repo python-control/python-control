misc bugfixes: fixed prewarp not working in c2d and sample_system, incorrect order of return arguments in margin, typos and changed to ControlMIMONotImplemented error where needed.

control/tests/matlab_test.py
@@ -361,7 +361,7 @@ class TestMatlab:
         gm, pm, wg, wp = margin(siso.ss2)
         gm, pm, wg, wp = margin(siso.ss2 * siso.ss2 * 2)
         np.testing.assert_array_almost_equal(
-            [gm, pm, wg, wp], [1.5451, 75.9933, 1.2720, 0.6559], decimal=3)
+            [gm, pm, wg, wp], [1.5451, 75.9933, 0.6559, 1.2720], decimal=3)
 
     def testDcgain(self, siso):
         """Test dcgain() for SISO system"""
@@ -785,8 +785,8 @@ class TestMatlab:
         # print("%f %f %f %f" % (gm, pm, wg, wp))
         np.testing.assert_allclose(gm, 3.32065569155)
         np.testing.assert_allclose(pm, 46.9740430224)
-        np.testing.assert_allclose(wg, 0.176469728448)
-        np.testing.assert_allclose(wp, 0.0616288455466)
+        np.testing.assert_allclose(wg, 0.0616288455466)
+        np.testing.assert_allclose(wp, 0.176469728448)
 
     def test_tf_string_args(self):
         """Make sure s and z are defined properly"""