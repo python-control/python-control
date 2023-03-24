039b22d29031c17b5a3dc7ae957b78fbd68519e5
Richard Murray <murray@cds.caltech.edu>
2023-03-23 21:55:40 -0700

    remove pytest --skipslow; use pytest -m "not slow" instead

--- control/tests/conftest.py
+++ control/tests/conftest.py
@@ -121,25 +121,6 @@ def mplcleanup():
         mpl.pyplot.close("all")
 
 
-#
-# Functionality to skip slow tests using --skipslow
-#
-# See https://docs.pytest.org/en/latest/example/simple.html
-#   #control-skipping-of-tests-according-to-command-line-option
-#
-def pytest_addoption(parser):
-    parser.addoption(
-        "--skipslow", action="store_true", default=False,
-        help="skip slow tests")
-
-
+# Allow pytest.mark.slow to mark slow tests (skip with pytest -m "not slow")
 def pytest_configure(config):
     config.addinivalue_line("markers", "slow: mark test as slow to run")
-
-
-def pytest_collection_modifyitems(config, items):
-    if config.getoption("--skipslow"):
-        skip_slow = pytest.mark.skip(reason="skipping slow tests")
-        for item in items:
-            if "slow" in item.keywords:
-                item.add_marker(skip_slow)
