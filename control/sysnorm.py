6f2a1ad90a6ea4e883593a84efed091d9452ad68
Henrik Sandberg <hsan@kth.se>
2024-01-21 19:29:32 +0100

    Added: * Use of warnings package. * Use routine statesp.linfnorm when slycot installed. * New routine internal _h2norm_slycot when slycot is installed.

--- control/sysnorm.py
+++ control/sysnorm.py
@@ -12,7 +12,9 @@ Author: Henrik Sandberg
 """
 
 import numpy as np
+import scipy as sp
 import numpy.linalg as la
+import warnings
 
 import control as ct
 
@@ -20,7 +22,68 @@ __all__ = ['norm']
 
 #------------------------------------------------------------------------------
 
-def norm(system, p=2, tol=1e-6, print_warning=True):
+def _h2norm_slycot(sys, print_warning=True):
+    """H2 norm of a linear system. For internal use. Requires Slycot.
+
+    See also
+    --------
+    slycot.ab13bd : the Slycot routine that does the calculation
+    https://github.com/python-control/Slycot/issues/199 : Post on issue with ab13bf
+    """
+    
+    try:
+        from slycot import ab13bd
+    except ImportError:
+        ct.ControlSlycot("Can't find slycot module 'ab13bd'!")
+
+    try:
+        from slycot.exceptions import SlycotArithmeticError
+    except ImportError: 
+        raise ct.ControlSlycot("Can't find slycot class 'SlycotArithmeticError'!")
+
+    A, B, C, D = ct.ssdata(ct.ss(sys))
+
+    n = A.shape[0]
+    m = B.shape[1]
+    p = C.shape[0]
+        
+    dico = 'C' if sys.isctime() else 'D'  # Continuous or discrete time
+    jobn = 'H'  # H2 (and not L2 norm)  
+
+    if n == 0:
+    # ab13bd does not accept empty A, B, C
+        if dico == 'C':
+            if any(D.flat != 0):
+                if print_warning:
+                    warnings.warn("System has a direct feedthrough term!")
+                return float("inf")
+            else:
+                return 0.0
+        elif dico == 'D':
+            return np.sqrt(D@D.T)
+  
+    try:
+        norm = ab13bd(dico, jobn, n, m, p, A, B, C, D)
+    except SlycotArithmeticError as e:
+        if e.info == 3:
+            if print_warning:
+                warnings.warn("System has pole(s) on the stability boundary!")
+            return float("inf")
+        elif e.info == 5:
+            if print_warning:
+                warnings.warn("System has a direct feedthrough term!")
+            return float("inf")
+        elif e.info == 6:
+            if print_warning:
+                warnings.warn("System is unstable!")
+            return float("inf")
+        else:
+            raise e
+    return norm
+
+#------------------------------------------------------------------------------
+
+def norm(system, p=2, tol=1e-10, print_warning=True, use_slycot=True):
     """Computes norm of system.
     
     Parameters
@@ -28,12 +91,14 @@ def norm(system, p=2, tol=1e-6, print_warning=True):
     system : LTI (:class:`StateSpace` or :class:`TransferFunction`)
         System in continuous or discrete time for which the norm should be computed.
     p : int or str
-        Type of norm to be computed. p=2 gives the H_2 norm, and p='inf' gives the L_infinity norm.
+        Type of norm to be computed. p=2 gives the H2 norm, and p='inf' gives the L-infinity norm.
     tol : float
-        Relative tolerance for accuracy of L_infinity norm computation. Ignored
+        Relative tolerance for accuracy of L-infinity norm computation. Ignored
         unless p='inf'.
     print_warning : bool
         Print warning message in case norm value may be uncertain.
+    use_slycot : bool
+        Use Slycot routines if available.
     
     Returns
     -------
@@ -42,7 +107,7 @@ def norm(system, p=2, tol=1e-6, print_warning=True):
    
     Notes
     -----
-    Does not yet compute the L_infinity norm for discrete time systems with pole(s) in z=0.
+    Does not yet compute the L-infinity norm for discrete time systems with pole(s) in z=0 unless Slycot is used.
     
     Examples
     --------
@@ -58,123 +123,163 @@ def norm(system, p=2, tol=1e-6, print_warning=True):
     C = G.C
     D = G.D
     
-    #
-    # H_2-norm computation
-    #
+    # -------------------
+    # H2 norm computation
+    # -------------------
     if p == 2:  
+        # --------------------
         # Continuous time case
+        # --------------------
         if G.isctime():
+            
+            # Check for cases with infinite norm
             poles_real_part = G.poles().real    
-            if any(np.isclose(poles_real_part, 0.0)):
+            if any(np.isclose(poles_real_part, 0.0)):  # Poles on imaginary axis
                 if print_warning:
-                    print("Warning: Poles close to, or on, the imaginary axis. Norm value may be uncertain.")
+                    warnings.warn("Poles close to, or on, the imaginary axis. Norm value may be uncertain.")
                 return float('inf')
-            elif (D != 0).any() or any(poles_real_part > 0.0):  # System unstable or has direct feedthrough?
+            elif any(poles_real_part > 0.0):  # System unstable
+                if print_warning:
+                    warnings.warn("System is unstable!")
                 return float('inf')
-            else:
-                try:
-                    P = ct.lyap(A, B@B.T)
-                except Exception as e:
-                    print(f"An error occurred solving the continuous time Lyapunov equation: {e}")
-                    return None
+            elif any(D.flat != 0):  # System has direct feedthrough
+                if print_warning:
+                    warnings.warn("System has a direct feedthrough term!")
+                return float('inf')   
+            
+            else:      
+                # Use slycot, if available, to compute (finite) norm
+                if ct.slycot_check() and use_slycot:
+                    return _h2norm_slycot(G, print_warning)  
                 
-                # System is stable to reach this point, and P should be positive semi-definite. 
-                # Test next is a precaution in case the Lyapunov equation is ill conditioned.
-                if any(la.eigvals(P) < 0.0):  
-                    if print_warning:
-                        print("Warning: There appears to be poles close to the imaginary axis. Norm value may be uncertain.")
-                    return float('inf')
+                # Else use scipy 
                 else:
-                    norm_value = np.sqrt(np.trace(C@P@C.T))  # Argument in sqrt should be non-negative
-                    if np.isnan(norm_value):
-                        print("Unknown error. Norm computation resulted in NaN.")           
-                        return None
+                    P = ct.lyap(A, B@B.T)  # Solve for controllability Gramian
+                                        
+                    # System is stable to reach this point, and P should be positive semi-definite. 
+                    # Test next is a precaution in case the Lyapunov equation is ill conditioned.
+                    if any(la.eigvals(P).real < 0.0):  
+                        if print_warning:
+                            warnings.warn("There appears to be poles close to the imaginary axis. Norm value may be uncertain.")
+                        return float('inf')
                     else:
-                        return norm_value
+                        norm_value = np.sqrt(np.trace(C@P@C.T))  # Argument in sqrt should be non-negative
+                        if np.isnan(norm_value):
+                            raise ct.ControlArgument("Norm computation resulted in NaN.")           
+                        else:
+                            return norm_value
         
+        # ------------------
         # Discrete time case
+        # ------------------
         elif G.isdtime():
+            
+            # Check for cases with infinite norm
             poles_abs = abs(G.poles())
-            if any(np.isclose(poles_abs, 1.0)):
+            if any(np.isclose(poles_abs, 1.0)):  # Poles on imaginary axis
                 if print_warning:
-                    print("Warning: Poles close to, or on, the complex unit circle. Norm value may be uncertain.")
+                    warnings.warn("Poles close to, or on, the complex unit circle. Norm value may be uncertain.")
                 return float('inf')
-            elif any(poles_abs > 1.0):  # System unstable?
+            elif any(poles_abs > 1.0):  # System unstable
+                if print_warning:
+                    warnings.warn("System is unstable!")
                 return float('inf')
+            
             else:
-                try:
+                # Use slycot, if available, to compute (finite) norm
+                if ct.slycot_check() and use_slycot:
+                    return _h2norm_slycot(G, print_warning)  
+                
+                # Else use scipy 
+                else:
                     P = ct.dlyap(A, B@B.T)
-                except Exception as e:
-                    print(f"An error occurred solving the discrete time Lyapunov equation: {e}")
-                    return None
                 
                 # System is stable to reach this point, and P should be positive semi-definite. 
                 # Test next is a precaution in case the Lyapunov equation is ill conditioned.
-                if any(la.eigvals(P) < 0.0):
+                if any(la.eigvals(P).real < 0.0):
                     if print_warning:
-                        print("Warning: There appears to be poles close to the complex unit circle. Norm value may be uncertain.")
+                        warnings.warn("Warning: There appears to be poles close to the complex unit circle. Norm value may be uncertain.")
                     return float('inf')
                 else:
                     norm_value = np.sqrt(np.trace(C@P@C.T + D@D.T))  # Argument in sqrt should be non-negative               
                     if np.isnan(norm_value):
-                        print("Unknown error. Norm computation resulted in NaN.")           
-                        return None
+                        raise ct.ControlArgument("Norm computation resulted in NaN.")    
                     else:
-                        return norm_value               
-    #
-    # L_infinity-norm computation
-    #
+                        return norm_value   
+                    
+    # ---------------------------
+    # L-infinity norm computation
+    # ---------------------------
     elif p == "inf":   
-        def _Hamilton_matrix(gamma):
-            """Constructs Hamiltonian matrix. For internal use."""
-            R = Ip*gamma**2 - D.T@D
-            invR = la.inv(R)
-            return np.block([[A+B@invR@D.T@C, B@invR@B.T], [-C.T@(Ip+D@invR@D.T)@C, -(A+B@invR@D.T@C).T]])    
-    
-        # Discrete time case 
-        # Use inverse bilinear transformation of discrete time system to s-plane if no poles on |z|=1 or z=0.
-        # Allows us to use test for continuous time systems next.
-        if G.isdtime():
-            Ad = A
-            Bd = B
-            Cd = C
-            Dd = D
-            if any(np.isclose(abs(la.eigvals(Ad)), 1.0)):
+        
+        # Check for cases with infinite norm
+        poles = G.poles()
+        if G.isdtime():  # Discrete time
+            if any(np.isclose(abs(poles), 1.0)):  # Poles on unit circle
                 if print_warning:
-                    print("Warning: Poles close to, or on, the complex unit circle. Norm value may be uncertain.")
+                    warnings.warn("Poles close to, or on, the complex unit circle. Norm value may be uncertain.")
                 return float('inf')
-            elif any(np.isclose(la.eigvals(Ad), 0.0)):
-                print("L_infinity norm computation for discrete time system with pole(s) at z=0 currently not supported.")            
-                return None
-            
-            # Inverse bilinear transformation
-            In = np.eye(len(Ad))
-            Adinv = la.inv(Ad+In)
-            A = 2*(Ad-In)@Adinv
-            B = 2*Adinv@Bd
-            C = 2*Cd@Adinv
-            D = Dd - Cd@Adinv@Bd
-        
-        # Continus time case 
-        if any(np.isclose(la.eigvals(A).real, 0.0)):
-            if print_warning:
-                print("Warning: Poles close to, or on, imaginary axis. Norm value may be uncertain.")
-            return float('inf')
+        else:  # Continuous time
+            if any(np.isclose(poles.real, 0.0)):  # Poles on imaginary axis
+                if print_warning:
+                    warnings.warn("Poles close to, or on, the imaginary axis. Norm value may be uncertain.")
+                return float('inf')
     
-        gaml = la.norm(D,ord=2)    # Lower bound
-        gamu = max(1.0, 2.0*gaml)  # Candidate upper bound
-        Ip = np.eye(len(D))    
-         
-        while any(np.isclose(la.eigvals(_Hamilton_matrix(gamu)).real, 0.0)):  # Find actual upper bound
-            gamu *= 2.0
+        # Use slycot, if available, to compute (finite) norm
+        if ct.slycot_check() and use_slycot:
+            return ct.linfnorm(G, tol)[0]
         
-        while (gamu-gaml)/gamu > tol:
-            gam = (gamu+gaml)/2.0
-            if any(np.isclose(la.eigvals(_Hamilton_matrix(gam)).real, 0.0)):
-                gaml = gam
-            else:
-                gamu = gam
-        return gam
+        # Else use scipy
+        else:
+        
+            # ------------------   
+            # Discrete time case
+            # ------------------
+            # Use inverse bilinear transformation of discrete time system to s-plane if no poles on |z|=1 or z=0.
+            # Allows us to use test for continuous time systems next.
+            if G.isdtime():
+                Ad = A
+                Bd = B
+                Cd = C
+                Dd = D
+                if any(np.isclose(la.eigvals(Ad), 0.0)):
+                    raise ct.ControlArgument("L-infinity norm computation for discrete time system with pole(s) in z=0 currently not supported unless Slycot installed.")            
+                
+                # Inverse bilinear transformation
+                In = np.eye(len(Ad))
+                Adinv = la.inv(Ad+In)
+                A = 2*(Ad-In)@Adinv
+                B = 2*Adinv@Bd
+                C = 2*Cd@Adinv
+                D = Dd - Cd@Adinv@Bd
+            
+            # --------------------
+            # Continuous time case
+            # --------------------
+            def _Hamilton_matrix(gamma):
+                """Constructs Hamiltonian matrix. For internal use."""
+                R = Ip*gamma**2 - D.T@D
+                invR = la.inv(R)
+                return np.block([[A+B@invR@D.T@C, B@invR@B.T], [-C.T@(Ip+D@invR@D.T)@C, -(A+B@invR@D.T@C).T]])        
+
+            gaml = la.norm(D,ord=2)    # Lower bound
+            gamu = max(1.0, 2.0*gaml)  # Candidate upper bound
+            Ip = np.eye(len(D))    
+             
+            while any(np.isclose(la.eigvals(_Hamilton_matrix(gamu)).real, 0.0)):  # Find actual upper bound
+                gamu *= 2.0
+            
+            while (gamu-gaml)/gamu > tol:
+                gam = (gamu+gaml)/2.0
+                if any(np.isclose(la.eigvals(_Hamilton_matrix(gam)).real, 0.0)):
+                    gaml = gam
+                else:
+                    gamu = gam
+            return gam
+    
+    # ----------------------
+    # Other norm computation
+    # ----------------------
     else:
-        print(f"Norm computation for p={p} currently not supported.")           
-        return None
+        raise ct.ControlArgument(f"Norm computation for p={p} currently not supported.")           
+
