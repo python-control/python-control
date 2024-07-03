a03ed38c1dbfefe68c084a8239c3465a90e424d4
Johannes Kaisinger <johannes.kaisinger@gmail.com>
2024-07-03 20:42:18 +0200

    Fix impuse response, input data only contains one inpute[0]==1 in each trace

--- control/timeresp.py
+++ control/timeresp.py
@@ -1960,7 +1960,7 @@ def impulse_response(sys, T=None, input=None, output=None, T_num=None,
         yout[:, inpidx, :] = response.y if output is None \
             else response.y[output]
         xout[:, inpidx, :] = response.x
-        uout[:, inpidx, :] = U[i]
+        uout[:, inpidx, :] = U if input is None else U[i]
 
     # Figure out if the system is SISO or not
     issiso = sys.issiso() or (input is not None and output is not None)
