3697afa9fbb3429a3f975ea376a95d262d2390fe
Richard Murray <murray@cds.caltech.edu>
2024-08-03 02:19:45 -0700

    improve docstrings unit test for deprecation, seq args; skip duplicates

--- control/tests/docstrings_test.py
+++ control/tests/docstrings_test.py
@@ -17,9 +17,21 @@ import control
 import control.flatsys
 
 # List of functions that we can skip testing (special cases)
-skiplist = [
+function_skiplist = [
     control.ControlPlot.reshape,                # needed for legacy interface
+    control.phase_plot,                         # legacy function
 ]
+
+# List of keywords that we can skip testing (special cases)
+keyword_skiplist = {
+    control.input_output_response: ['method'],
+    control.nyquist_plot: ['color'],            # checked separately
+    control.optimal.solve_ocp: ['method'],      # deprecated
+    control.sisotool: ['kvect'],                # deprecated
+}
+
+# Decide on the level of verbosity (use -rP when running pytest)
+verbose = 1
 
 @pytest.mark.parametrize("module, prefix", [
     (control, ""), (control.flatsys, "flatsys."),
@@ -27,61 +39,136 @@ skiplist = [
 ])
 def test_docstrings(module, prefix):
     # Look through every object in the package
-    print(f"Checking module {module}")
+    if verbose > 1:
+        print(f"Checking module {module}")
     for name, obj in inspect.getmembers(module):
         # Skip anything that is outside of this module
-        if inspect.getmodule(obj) is not None and \
-           not inspect.getmodule(obj).__name__.startswith('control'):
+        if inspect.getmodule(obj) is not None and (
+                not inspect.getmodule(obj).__name__.startswith('control')
+                or prefix != "" and inspect.getmodule(obj) != module):
             # Skip anything that isn't part of the control package
             continue
 
         if inspect.isclass(obj):
-            print(f"  Checking class {name}")
+            if verbose > 1:
+                print(f"  Checking class {name}")
             # Check member functions within the class
-            test_docstrings(obj, prefix + obj.__name__ + '.')
+            test_docstrings(obj, prefix + name + '.')
 
         if inspect.isfunction(obj):
-            # Skip anything that is inherited or hidden
-            if inspect.isclass(module) and obj.__name__ not in module.__dict__ \
-               or obj.__name__.startswith('_') or obj in skiplist:
+            # Skip anything that is inherited, hidden, or deprecated
+            if inspect.isclass(module) and name not in module.__dict__ \
+               or name.startswith('_') or obj in function_skiplist:
                 continue
 
-            # Make sure there is a docstring
-            print(f"  Checking function {name}")
+            # Get the docstring (skip w/ warning if there isn't one)
+            if verbose > 1:
+                print(f"  Checking function {name}")
             if obj.__doc__ is None:
                 warnings.warn(
-                    f"{module.__name__}.{obj.__name__} is missing docstring")
+                    f"{module.__name__}.{name} is missing docstring")
                 continue
-            
+            else:
+                docstring = inspect.getdoc(obj)
+                source = inspect.getsource(obj)
+
+            # Skip deprecated functions
+            if f"{name} is deprecated" in docstring or \
+               "function is deprecated" in docstring or \
+               ".. deprecated::" in docstring:
+                if verbose > 1:
+                    print("    [deprecated]")
+                continue
+
+            elif f"{name} is deprecated" in source:
+                if verbose:
+                    print(f"    {name} is deprecated, but not documented")
+                warnings.warn(f"{name} deprecated, but not documented")
+                continue
+
             # Get the signature for the function
             sig = inspect.signature(obj)
 
             # Go through each parameter and make sure it is in the docstring
             for argname, par in sig.parameters.items():
-                if argname == 'self' or argname[0] == '_':
+
+                # Look for arguments that we can skip
+                if argname == 'self' or argname[0] == '_' or \
+                   obj in keyword_skiplist and argname in keyword_skiplist[obj]:
                     continue
-                
-                if par.kind == inspect.Parameter.VAR_KEYWORD:
-                    # Found a keyword argument; look at code for parsing
-                    warnings.warn("keyword argument checks not yet implemented")
+
+                # Check for positional arguments
+                if par.kind == inspect.Parameter.VAR_POSITIONAL:
+                    # Too complicated to check
+                    if f"*{argname}" not in docstring and verbose:
+                        print(f"      {name} has positional arguments; "
+                              "check manually")
+                    continue
+
+                # Check for keyword arguments (then look at code for parsing)
+                elif par.kind == inspect.Parameter.VAR_KEYWORD:
+                    # See if we documented the keyward argumnt directly
+                    if f"**{argname}" in docstring:
+                        continue
+
+                    # Look for direct kwargs argument access
+                    kwargnames = set()
+                    for _, kwargname in re.findall(
+                            argname + r"(\[|\.pop\(|\.get\()'([\w]+)'",
+                            source):
+                        if verbose > 2:
+                            print("    Found direct keyword argument",
+                                  kwargname)
+                        kwargnames.add(kwargname)
+
+                    # Look for kwargs access via _process_legacy_keyword
+                    for kwargname in re.findall(
+                            r"_process_legacy_keyword\([\s]*" + argname +
+                            r",[\s]*'[\w]+',[\s]*'([\w]+)'", source):
+                        if verbose > 2:
+                            print("    Found legacy keyword argument",
+                                  {kwargname})
+                        kwargnames.add(kwargname)
+
+                    for kwargname in kwargnames:
+                        if obj in keyword_skiplist and \
+                           kwargname in keyword_skiplist[obj]:
+                            continue
+                        if verbose > 3:
+                            print(f"    Checking keyword argument {kwargname}")
+                        assert _check_docstring(
+                            name, kwargname, inspect.getdoc(obj),
+                            prefix=prefix)
 
                 # Make sure this argument is documented properly in docstring
                 else:
-                    assert _check_docstring(obj.__name__, argname, obj.__doc__)
+                    if verbose > 3:
+                        print(f"    Checking argument {argname}")
+                    assert _check_docstring(
+                        name, argname, docstring, prefix=prefix)
 
 
 # Utility function to check for an argument in a docstring
-def _check_docstring(funcname, argname, docstring):
-    if re.search(f"    ([ \\w]+, )*{argname}(,[ \\w]+)*[^ ]:", docstring):
+def _check_docstring(funcname, argname, docstring, prefix=""):
+    funcname = prefix + funcname
+    if re.search(
+            "\n" + r"((\w+|\.{3}), )*" + argname + r"(, (\w+|\.{3}))*:",
+            docstring):
         # Found the string, but not in numpydoc form
+        if verbose:
+            print(f"      {funcname}: {argname} docstring missing space")
         warnings.warn(f"{funcname} '{argname}' docstring missing space")
         return True
-    
-    elif not re.search(f"    ([ \\w]+, )*{argname}(,[ \\w]+)* :", docstring):
+
+    elif not re.search(
+            "\n" + r"((\w+|\.{3}), )*" + argname + r"(, (\w+|\.{3}))* :",
+            docstring):
         # return False
         #
         # Just issue a warning for now
+        if verbose:
+            print(f"      {funcname}: {argname} not documented")
         warnings.warn(f"{funcname} '{argname}' not documented")
         return True
-    
+
     return True
