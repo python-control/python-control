37ccb27086e5ea624240031d0dbc5c5cac0a7d2e
Vaibhav Gupta <vaibhav.gupta@epfl.ch>
2025-09-09 11:31:38 +0200

    BugFix: `place_acker` output should be 2D matrix

--- control/statefbk.py
+++ control/statefbk.py
@@ -252,7 +252,7 @@ def place_acker(A, B, poles):
     K = np.linalg.solve(ct, pmat)
 
     K = K[-1, :]                # Extract the last row
-    return K
+    return _ssmatrix(K)
 
 
 def lqr(*args, **kwargs):
